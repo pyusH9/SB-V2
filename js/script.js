0f44fd098af0ac3e0a97160eb8149809700aa5c2
pyusH9 <024neb943@sxc.edu.np>
2026-02-15 17:29:22 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,3 +49,18 @@ setInterval(() => {
     index = (index + 1) % totalCards;
     moveCarousel();
 }, 3000); // moves every 3 seconds
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = { threshold: 0.2 };
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('visible');
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
